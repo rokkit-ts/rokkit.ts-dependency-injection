efb5e46cc8bac236434793abbf03fff5cdc4d6c6
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-09-01 22:33:24 +0200

    updates dependencyInjectionContext and its test

--- src/dependency-injection-assembler/dependencyInjectionContext.ts
+++ src/dependency-injection-assembler/dependencyInjectionContext.ts
@@ -5,6 +5,9 @@ export class DependencyInjectionContext {
   private injectors: Map<string, Injector<any>>;
 
   constructor(contextName: string) {
+    if (contextName.length === 0) {
+      throw new Error("Context name should not be empty.");
+    }
     this.contextName = contextName;
     this.injectors = new Map<string, Injector<any>>();
   }
@@ -13,18 +16,20 @@ export class DependencyInjectionContext {
     return this.contextName;
   }
 
-  addInjector<T extends object>(
+  public addInjector<T extends object>(
     injectorName: string,
     injector: Injector<T>
   ): void {
     this.injectors.set(injectorName, injector);
   }
 
-  getInjector<T extends object>(injectorName: string): Injector<T> | undefined {
+  public getInjector<T extends object>(
+    injectorName: string
+  ): Injector<T> | undefined {
     return this.injectors.get(injectorName);
   }
 
-  getAllInjectors(): Injector<any>[] {
+  public getAllInjectors(): Injector<any>[] {
     const injectors: Injector<any>[] = [];
     this.injectors.forEach(injector => injectors.push(injector));
     return injectors;
